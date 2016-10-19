01cbfde27be6385c15e876d7e7a158d7d5498cdb
LawlietBlack <Kenneth.Black@live.com>
2016-10-19 16:07:28 -0700

    refactored data model and processing logic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import Track from './Track';
 import {browserHistory} from 'react-router';
-import trackData from '../../data/trackData.json';
 import {processUserData} from '../helpers/treehouse';
 
 export default class Main extends React.Component {
@@ -10,70 +9,39 @@ export default class Main extends React.Component {
     super();
     this.state = {
       username: "",
-      userData: {},
-      badgesByClass: {}
+      userData: null
     };
-    this.getBadgesByClass = this.getBadgesByClass.bind(this);
     this.getUserData = this.getUserData.bind(this);
-  }
-  getBadgesByClass(badges) {
-    let badgeData = {};
-    for(let i=0; i<badges.length; i++) {
-      if(badges[i].courses.length > 0) {
-        let title = badges[i].courses[0].title;
-        badgeData[title] = (badgeData[title] === undefined) ? 1 : badgeData[title] + 1
-      }
-    }
-    return badgeData
-  }
-  getCompletedClassCount(trackName) {
-    return trackData[trackName].map(course => course.title)
-      .filter(course => !!this.state.badgeData[course]).length;
-  }
-  getCompletedTracks() {
-    let trackNames = Object.keys(trackData);
-    return trackNames.filter(trackName =>
-      this.getCompletedClassCount(trackName) === trackData[trackName].length);
   }
   getUserData(username) {
     console.log("NEW USER: " + username);
     axios.get(`https://teamtreehouse.com/${username}.json`).then(res => {
-      processUserData(res.data);
       this.setState({
         username: username,
-        userData: res.data,
-        badgeData: this.getBadgesByClass(res.data.badges)
+        userData: processUserData(res.data)
       });
-    }).catch(err => {
-      console.log(err);
     });
   }
-  componentWillMount() {
-    if(this.params && this.params.username) this.getUserData(this.params.username);
-    else this.getUserData("kennethblack");
+  componentDidMount() {
+    console.log(window.location);
+    let username = window.location.hash.split(/[?/]/)[1];
+    if(username.indexOf('?') >=0) username = 'kennethblack';
+    this.getUserData(username);
   }
   componentWillReceiveProps(next) {
     if(next.params.username && next.params.username !== this.state.username) this.getUserData(next.params.username);
   }
   render() {
+    console.log(this.state.userData);
+    if(!this.state.userData) return <main>Loading</main>;
     return <main>
-      {this.state.badgeData && <h1>{this.state.username}'s Track Completion</h1>}
-      {trackData && this.state.badgeData && <h3>Completed Tracks: {this.getCompletedTracks().length}</h3>}
-      {this.state.badgeData && trackData &&
-      (this.props.params.track && trackData[this.props.params.track]
-        ? <Track title={this.props.params.track}
-                 badgeData={this.state.badgeData}
-                 params={this.props.params}
-                 completedCount={this.getCompletedClassCount(this.props.params.track)}
-                 courses={trackData[this.props.params.track]}/>
-        : Object.keys(trackData).filter(trackName => this.getCompletedClassCount(trackName) > 2)
-          .map((trackName, i) =>
-          <Track title={trackName}
-                 badgeData={this.state.badgeData}
-                 params={this.props.params}
-                 completedCount={this.getCompletedClassCount(trackName)}
-                 courses={trackData[trackName]} key={i}/>)
-      )}
+      <h1>{this.state.username}'s Track Completion</h1>
+      <h3>Completed Tracks: {this.state.userData.completedTracks.length}</h3>
+      {this.props.params.track
+      ? <Track trackData={this.state.userData.tracks.filter(t => t.title === this.props.params.track)[0]}
+               params={this.props.params} />
+      : this.state.userData.tracks.filter(track => track.progress > 2).map((track, i) =>
+        <Track trackData={track} params={this.props.params} key={i}/>)}
     </main>;
   }
 }
--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -3,18 +3,36 @@ import {Link} from 'react-router';
 
 const Track = (props) => {
   let root = props.params.username ? `/${props.params.username}/` : '/';
+  if(!props.params.track)
+    return <p><Link to={`${root}tracks/${props.trackData.title}`} >{props.trackData.title} Track
+      <span className="progress">{props.trackData.progress === props.trackData.count
+        ? " (Complete)"
+        : ` (${props.trackData.progress}/${props.trackData.count})`}
+      </span></Link></p>;
   return (<div className="track-data">
-    {props.params.track && <Link to={`${root}`} ><h1>Tracks Home</h1></Link>}
-    {props.params.track
-      ? <h1>{props.title} Track Completion</h1>
-      : <h1><Link to={`${root}tracks/${props.title}`} >{props.title} Track Completion</Link></h1>}
-    <h4>Courses: {props.completedCount}/{props.courses.filter(c => c.type !== 'Workshop').length}</h4>
-    {props.courses.map((course, i) => <div className="course" key={i}>
-      <h4>{course.title} {course.type === 'Workshop' ? "(Workshop)"
-        : `${props.badgeData[course.title] || 0}/${course.count}`}</h4>
-
-    </div>)}
+    <Link to={`${root}`} ><h1>Tracks Home</h1></Link>
+    <h1>{props.trackData.title} Track Completion</h1>
+    <h2>Track Progress: {`${props.trackData.progress}/${props.trackData.count}`}</h2>
+    {props.trackData.courses.map((course, i) =>
+      <div className="course" key={i}>
+        <h4>{course.title} {course.type === 'Workshop'
+          ? "(Workshop)"
+          : `(${course.progress}/${course.count})`}</h4>
+      </div>
+    )}
   </div>)
 };
 
 export default Track;
+
+//
+// {props.params.track && <Link to={`${root}`} ><h1>Tracks Home</h1></Link>}
+// {props.params.track
+//   ? <h1>{props.title} Track Completion</h1>
+//   : <h1><Link to={`${root}tracks/${props.title}`} >{props.title} Track Completion</Link></h1>}
+// <h4>Courses: {props.completedCount}/{props.courses.filter(c => c.type !== 'Workshop').length}</h4>
+// {props.courses.map((course, i) => <div className="course" key={i}>
+//   <h4>{course.title} {course.type === 'Workshop' ? "(Workshop)"
+//     : `${props.badgeData[course.title] || 0}/${course.count}`}</h4>
+//
+// </div>)}
